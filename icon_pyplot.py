12ddaff5b96c36662fa4527f111cdf52279e5efe
Jan Kretzschmar <jan.kretzschmar@uni-leipzig.de>
2020-04-24 16:32:17 +0200

    Some small adaptions

--- icon_pyplot.py
+++ icon_pyplot.py
@@ -27,32 +27,32 @@ m.drawcoastlines(linewidth=1.7)
 m.drawcountries(linewidth=1.7)
 
 
-# # use this for contour plot with filled triangles
-# def create_triagulation_triangles(ifile_grid_in):
-#     f_grid = Dataset(ifile_grid_in, 'r')
-#     triangles = np.swapaxes(f_grid.variables['vertex_of_cell'][:]-1, 0, 1)
-#     vlon, vlat = np.degrees(f_grid.variables['vlon'][:]), np.degrees(f_grid.variables['vlat'][:])
-#     triang_out = tri.Triangulation(vlon, vlat, triangles)
-#     return triang_out
-#
-#
-# triang = create_triagulation_triangles(ipath+ifile_grid)
-# plt.tripcolor(triang, facecolors=var, edgecolors='k')
-
+# use this for contour plot with filled triangles. You need the grid file for that
+def create_triagulation_triangles(ifile_grid_in):
+    f_grid = Dataset(ifile_grid_in, 'r')
+    triangles = np.swapaxes(f_grid.variables['vertex_of_cell'][:]-1, 0, 1)
+    vlon, vlat = np.degrees(f_grid.variables['vlon'][:]), np.degrees(f_grid.variables['vlat'][:])
+    triang_out = tri.Triangulation(vlon, vlat, triangles)
+    return triang_out
+triang = create_triagulation_triangles(ipath+ifile_grid)
+plt.tripcolor(triang, facecolors=var, edgecolors='k')
+plt.show()
 
 # use this for a smoothed contour plot
+
+fig = plt.figure(figsize=(12, 13))
+m = Basemap(projection='cyl', llcrnrlon=5.5, llcrnrlat=47.2, urcrnrlon=15.3, urcrnrlat=55, resolution='i')
+m.drawcoastlines(linewidth=1.7)
+m.drawcountries(linewidth=1.7)
 def create_triagulation_smooth(ifile_grid_in):
     f_grid = Dataset(ipath+ifile_grid, 'r')
     clon, clat = np.degrees(f_grid.variables['clon'][:]), np.degrees(f_grid.variables['clat'][:])
     triang_out = tri.Triangulation(clon, clat)
     return triang_out
 
-
 triang = create_triagulation_smooth(ipath+ifile_grid)
 level = np.linspace(265, 285, 50)
 plt.tricontourf(triang, var, levels=level)
-
-
 cbar = plt.colorbar(fraction=0.036, )
 cbar.ax.tick_params(labelsize=30)
 plt.show()
