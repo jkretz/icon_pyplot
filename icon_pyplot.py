2ea26c3117df875c37f75089f828afea5f99ce9a
Iris Papakonstantinou Presvelou <iripapak@gmail.com>
2022-03-03 13:10:08 +0100

    fix colorbars

--- icon_pyplot.py
+++ icon_pyplot.py
@@ -19,6 +19,10 @@ ifile_grid = 'test_grid_DOM01.nc'
 var_string = 't_2m'
 f_var = Dataset(ipath+ifile_var, 'r')
 var = np.squeeze(f_var.variables[var_string][:])
+#print(type(var))
+level = np.linspace(265, 285, 50)
+labels= np.arange(265, 286, 1)
+
 
 # create basic map
 fig = plt.figure(figsize=(12, 13))
@@ -29,33 +33,44 @@ m.drawcountries(linewidth=1.7)
 
 # use this for contour plot with filled triangles. You need the grid file for that
 def create_triagulation_triangles(ifile_grid_in):
+
     f_grid = Dataset(ifile_grid_in, 'r')
     triangles = np.swapaxes(f_grid.variables['vertex_of_cell'][:]-1, 0, 1)
     vlon, vlat = np.degrees(f_grid.variables['vlon'][:]), np.degrees(f_grid.variables['vlat'][:])
     triang_out = tri.Triangulation(vlon, vlat, triangles)
     return triang_out
+
 triang = create_triagulation_triangles(ipath+ifile_grid)
-plt.tripcolor(triang, facecolors=var, edgecolors='k')
+plt.tripcolor(triang, facecolors=var, vmin=265, vmax=285, edgecolors='k')
+cbar = plt.colorbar(ticks=labels, fraction=0.036)
+cbar.ax.tick_params(labelsize=10)
+cbar.set_label(label='Temperature in K', rotation=90)
 plt.show()
+#plt.savefig('Plot_unstructured_triangles.png')
+
+##################################################################################
 
 # use this for a smoothed contour plot
-
 fig = plt.figure(figsize=(12, 13))
 m = Basemap(projection='cyl', llcrnrlon=5.5, llcrnrlat=47.2, urcrnrlon=15.3, urcrnrlat=55, resolution='i')
 m.drawcoastlines(linewidth=1.7)
 m.drawcountries(linewidth=1.7)
+
+
 def create_triagulation_smooth(ifile_grid_in):
+
     f_grid = Dataset(ipath+ifile_grid, 'r')
     clon, clat = np.degrees(f_grid.variables['clon'][:]), np.degrees(f_grid.variables['clat'][:])
     triang_out = tri.Triangulation(clon, clat)
     return triang_out
 
 triang = create_triagulation_smooth(ipath+ifile_grid)
-level = np.linspace(265, 285, 50)
-plt.tricontourf(triang, np.array(var), levels=level)
-cbar = plt.colorbar(fraction=0.036, )
-cbar.ax.tick_params(labelsize=30)
+
+# convert to numpy array or just take the data (maskarrays cannot be plotted by tricontourf)
+# plt.tricontourf(triang, np.array(var), levels=level)
+plt.tricontourf(triang, var.data, levels=level)
+cbar = plt.colorbar(ticks=labels, fraction=0.036)
+cbar.ax.tick_params(labelsize=10)
+cbar.set_label(label='Temperature in K', rotation=90)
 plt.show()
-
-
-exit()
+#plt.savefig('Plot_unstructured.png')
